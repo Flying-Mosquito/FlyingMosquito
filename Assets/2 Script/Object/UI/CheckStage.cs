36f4d1a96ce5c4619be708a41157f0bb76067cef
rezzad <rezzad1@naver.com>
2016-07-30 20:52:19 +0900

    map수정

--- Assets/2 Script/Object/UI/CheckStage.cs
+++ Assets/2 Script/Object/UI/CheckStage.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
 
 public class CheckStage : MonoBehaviour {
     public Transform Stage1;
@@ -11,26 +14,38 @@ public class CheckStage : MonoBehaviour {
     public Transform Stage7;
     public Transform Stage8;
     public Transform Stage9;
+    private int iStage = 9;
+    public List<Transform> Stage = new List<Transform>();
     public bool clear;
-
- 
+    private int MAXINDEX = 3;  
+    private int[] Arr = { };
+   
+    
     // Use this for initialization
     void Start () {
 
+        Arr = new int[iStage];
         clear = true;
-   
+        for(int i=0;i<9;i++)
+        {
+            Arr[i] = 0;//0닫힘 1주황 2 초록
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(clear==false)
+        for (int i = 0; i < 9; i++)
         {
-            Stage1.Find("1-1").gameObject.SetActive(true);
-          
-        }
-        if(clear==true)
-        {
-         
+
+            if (Arr[i]==2)
+            {
+                Stage1.Find("1-1").gameObject.SetActive(true);
+
+            }
+            if (clear == true)
+            {
+
+            }
         }
        
      
---
+++ Assets/2 Script/Object/UI/StarColor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class StarColor : MonoBehaviour {
+    public Transform star;
+    public Image image;
+    public Image image2;
+    public Image image3;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        image.color = Color.green;
+        image2.color = Color.green;
+        image3.color = Color.green;
+    }
+}
