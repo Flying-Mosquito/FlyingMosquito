48ba1f350a55313c1b6f751c49fdebc278d4e394
myocon <myo_con@naver.com>
2016-08-03 00:55:54 +0900

    Spider----작업중

---
+++ Assets/2 Script/Object/SpiderCtrl.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderCtrl : MonoBehaviour {
+    public Animator Anim;
+    public Transform tr;
+
+    public enum eState { IDLE, MOVE, ATTACK }
+    public eState state;
+
+    private float fDist;
+	// Use this for initialization
+	void Start ()
+    {
+        tr = GetComponent<Transform>();
+        Anim = GetComponent<Animator>();
+
+        state = eState.IDLE;
+        fDist = 40f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        print("fDist:" + (Vector3.Distance(tr.position, PlayerCtrl.Instance.transform.position)));
+        if (Vector3.Distance(tr.position, PlayerCtrl.Instance.transform.position) < fDist)
+            state = eState.ATTACK;
+        else
+            state = eState.IDLE;
+
+        Animate();
+	}
+
+    void Animate()
+    {
+        switch(state)
+        {
+            case eState.IDLE:
+                {
+                    Anim.SetInteger("iState",0);
+                    break;
+                }
+            case eState.MOVE:
+                {
+                    Anim.SetInteger("iState", 1);
+                    break;
+                }
+            case eState.ATTACK:
+                {
+                    Anim.SetInteger("iState", 2);
+                    break;
+                }
+                
+        }
+    }
+}
