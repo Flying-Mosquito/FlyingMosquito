a21f35768e9cd6d9ab963584ecd2f8da4b299bc2
myocon <myo_con@naver.com>
2016-08-09 15:18:34 +0900

    Chameleon

---
+++ Assets/2 Script/Object/ChameleonCtrl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChameleonCtrl : MonoBehaviour {
+
+    public enum eState { IDLE, EAT }
+    public eState state;
+    public Animator Anim;
+
+    public int iFoodNum; // 만족시켜야 하는 먹이 개수 
+    public int iFoodCount; // 먹은 먹이 개수 
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        Anim = GetComponent<Animator>();
+        state = eState.IDLE;
+        iFoodCount = 0;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       // Anim.GetCurrentAnimatorStateInfo
+	    if(Anim.GetAnimatorTransitionInfo(0).IsName("Base Layer.eat -> Base Layer.idle"))
+        {
+            state = eState.IDLE;
+            Anim.SetInteger("iState", 0);
+        }
+	}
+
+    void OnCollisionEnter(Collision _coll)
+    {
+        if(_coll.gameObject.CompareTag("CHAMELFOOD"))
+        {
+            ++iFoodCount;
+            Anim.SetInteger("iState", 1);
+            state = eState.EAT;
+
+            Destroy(_coll.gameObject);
+         //   Anim.Play("eat");
+        }
+    }
+
+}
