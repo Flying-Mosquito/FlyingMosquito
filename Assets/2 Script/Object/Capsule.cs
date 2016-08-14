e41d8e1b6ccd3bbb5e7a65d7129f4d2a9d9499a4
rezzad <rezzad1@naver.com>
2016-08-14 19:24:43 +0900

    추가
    ㅇ

---
+++ Assets/2 Script/Object/Capsule.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Capsule : MonoBehaviour {
+
+    public Transform open;
+
+    void OnCollisionEnter(Collision coll)
+    {
+
+        if (coll.gameObject.tag == "PLAYER")
+        {
+          
+
+            open.gameObject.SetActive(true);
+
+        }
+    }
+}
