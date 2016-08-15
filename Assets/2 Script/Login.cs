ee6ef4dda492884d068e4893e51f37e08a7ea40e
rezzad <rezzad1@naver.com>
2016-08-16 05:16:46 +0900

    수정

--- Assets/2 Script/Login.cs
+++ Assets/2 Script/Login.cs
@@ -1,21 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
 using UnityEngine.SceneManagement;
-public class Login : MonoBehaviour {
+
+public class Login : MonoBehaviour
+{
     #region Variables
     //Static Variables
-    public static string Email = "";
+    public static string Id = "";
     public static string Password = "";
+    public static string NickName = "";
     //Public Variables
     public string CurrentMenu = "Login";
     //Private Variables
-    private string CreateAccountUrl = "";
-   // private string LoginUrl = "";
+    private string CheckDupUrl = "http://192.168.1.105:8080/check_duplicate";
+    public string CreateAccountUrl = "http://192.168.1.105:8080/make_account";
+    private string CheckLoginUrl = "http://192.168.1.105:8080/check_login";
+    private string TakeDataUrl = "http://192.168.1.105:8080/get_data";
+    private string GetStageUrl = "http://192.168.1.105:8080/get_stage_list";
+    private string EnterStageUrl = "http://192.168.1.105:8080/enter_stage";
+    private string EndStageUrl = "http://192.168.1.105:8080/end_stage";
+
+    // private string LoginUrl = "";
     private string ConfrimPass = "";
     private string ConfirmEmail = "";
     private string CEmail = "";
     private string Cpassword = "";
-
+    private string Cnickname = "";
+    public string AccessNumber = "";
+    private int ActiveNum = 4;
     //GUI Test section
 
     public float X;
@@ -25,117 +41,165 @@ public class Login : MonoBehaviour {
     public float control = 60;
     #endregion 
     // Use this for initialization
-    void Start () {
-	
-	}
-    void OnGUI()
+    void Start()
     {
 
-        if(CurrentMenu =="Login")
+    }
+    void OnGUI()
+    {
+        WWW www = new WWW("http://");
+
+        if (CurrentMenu == "Login")
         {
-            LoginGUI();
-        }else if(CurrentMenu == "CreateAccount")
+            LoginGUI(www);
+        }
+        else if (CurrentMenu == "CreateAccount")
         {
-            CreateAccountGUI();
+            CreateAccountGUI(www);
         }
 
     }
 
     #region Custom methods
 
-    void LoginGUI()
+    void LoginGUI(WWW www)
     {
-        GUI.Box(new Rect(280-control*2, 120 - control, (Screen.width/4)+ 200,(Screen.height/4)+ 250),"Login");
+        GUI.Box(new Rect(280 - control * 2, 120 - control, (Screen.width / 4) + 200, (Screen.height / 4) + 250), "Login");
 
-        if(GUI.Button(new Rect(360 - control*2, 360 - control, 120,25),"Create Account"))
+        GUI.Label(new Rect(390 - control * 2, 200 - control, 220, 25), "ID");
+        Id = GUI.TextField(new Rect(390 - control * 2, 225 - control, 220, 25), Id);
+
+        GUI.Label(new Rect(390 - control * 2, 250 - control, 220, 25), "Password");
+        Password = GUI.TextField(new Rect(390 - control * 2, 275 - control, 220, 25), Password);
+
+
+        if (GUI.Button(new Rect(360 - control * 2, 360 - control, 120, 25), "Create Account"))
         {
             CurrentMenu = "CreateAccount";
         }
 
 
-        if (GUI.Button(new Rect(520 - control*2, 360 - control, 120, 25), "Log in"))
+        if (GUI.Button(new Rect(520 - control * 2, 360 - control, 120, 25), "Log in"))
         {
-        Application.LoadLevel(12);
+            WWWForm form = new WWWForm();
+            form.AddField("user_id", Id);
+            form.AddField("user_passwd", Password);
 
+            StartCoroutine(CreateAccount(www, CheckLoginUrl, form));
+
+            form = new WWWForm();
+            form.AddField("code", AccessNumber);
+            StartCoroutine(CreateAccount(www, TakeDataUrl, form));
+
+           
+                SceneManager.LoadScene(12);
+            
         }
-        GUI.Label(new Rect(300 - control*2, 200 - control, 220, 25), "Email");
-        Email = GUI.TextField(new Rect(390 - control*2, 225 - control, 220,25), Email);
-
-        GUI.Label(new Rect(300 - control*2, 250 - control, 220, 25), "Password");
-        Password = GUI.TextField(new Rect(390 - control*2, 275 - control, 220, 25), Password);
 
     }
     public void Click()
     {
 
-        if (GUI.Button(new Rect(520 - control*2, 360 - control, 120, 25), "Log in"))
+        if (GUI.Button(new Rect(520 - control * 2, 360 - control, 120, 25), "Log in"))
         {
-            SceneManager.LoadScene(12);
+            SceneManager.LoadScene(1);
 
         }
-       
-    }
-    void CreateAccountGUI() {
-        GUI.Box(new Rect(280 - control*2, 120 - control, (Screen.width / 4) + 200, (Screen.height / 4) + 250), "Create Account");
 
-     
-        GUI.Label(new Rect(370 - control*2, 200 - control, 220, 25), "Email");
+    }
+    void CreateAccountGUI(WWW www)
+    {
+        GUI.Box(new Rect(280 - control * 2, 120 - control, (Screen.width / 4) + 200, (Screen.height / 4) + 250), "Create Account");
+
+
+        GUI.Label(new Rect(390 - control * 2, 200 - control, 220, 25), "Id");
         CEmail = GUI.TextField(new Rect(390 - control * 2, 225 - control, 220, 25), CEmail);
 
-        GUI.Label(new Rect(370 - control * 2, 250 - control, 220, 25), "Password");
+        GUI.Label(new Rect(390 - control * 2, 250 - control, 220, 25), "Password");
         Cpassword = GUI.TextField(new Rect(390 - control * 2, 275 - control, 220, 25), Cpassword);
 
 
-        GUI.Label(new Rect(370 - control * 2, 320 - control, 220, 25), "Confirm Email");
-        ConfirmEmail = GUI.TextField(new Rect(390 - control * 2, 340 - control, 220, 25), ConfirmEmail);
-
-        GUI.Label(new Rect(370 - control * 2, 370 - control, 220, 25), "Confirm Password");
-       ConfrimPass = GUI.TextField(new Rect(390 - control * 2, 400 - control, 220, 25), ConfrimPass);
+        GUI.Label(new Rect(390 - control * 2, 320 - control, 220, 25), "NickName");
+        Cnickname = GUI.TextField(new Rect(390 - control * 2, 340 - control, 220, 25), Cnickname);
 
 
         if (GUI.Button(new Rect(360 - control * 2, 460 - control, 120, 25), "Create Account"))
         {
-            if (ConfrimPass == Cpassword && ConfirmEmail == CEmail) {
-                StartCoroutine("Create Account");
-            }
-            else
+            WWWForm form = new WWWForm();
+            form.AddField("user_id", CEmail);
+            form.AddField("user_nickname", Cnickname);
+
+            StartCoroutine(CreateAccount(www, CheckDupUrl, form));
+
+            if (ActiveNum == 1)
             {
-                StartCoroutine("Create Account");
+                form = new WWWForm();
+                form.AddField("user_id", CEmail);
+                form.AddField("user_nickname", Cnickname);
+                form.AddField("user_passwd", Cpassword);
+                StartCoroutine(CreateAccount(www, CreateAccountUrl, form));
+                if (ActiveNum == 5)
+                {
+                    CurrentMenu = "Login";
+                }
+            }
+            else if (ActiveNum == 0)
+            {
+                Debug.Log("There is same ID or Nickname already");
             }
         }
-        if (GUI.Button(new Rect(520 - control*2, 460 - control, 120, 25), "Back"))
+        if (GUI.Button(new Rect(520 - control * 2, 460 - control, 120, 25), "Back"))
         {
             CurrentMenu = "Login";
-
         }
     }
-  
+
     #endregion
 
     #region CoRoutines
-    //IEnumerator CreateAccount()
-    //{
-    //    WWWForm Form = new WWWForm();
-    //    Form.AddField("Email", CEmail);
-    //    Form.AddField("Password", Cpassword);
+   public IEnumerator CreateAccount(WWW www,  string url, WWWForm form)
+    {
 
-    //    WWW CreateAcountWWW = new WWW(CreateAccountUrl, Form);
+        www = new WWW(url, form);
 
-    //    yield return CreateAcountWWW;
-    //    if(CreateAcountWWW.error != null)
-    //    {
-    //        Debug.LogError("Cannot Connect to Account Creation");
-
-    //    }else
-    //    {
-    //        string CreateAccountReturn = CreateAcountWWW.text;
-    //        if(CreateAccountReturn == "Success")
-    //        {
-    //            Debug.Log("Success : Account Created");
-    //            CurrentMenu = "Login";
-
-    //        }
-    //    }
-    //}
+        yield return www;
+        if (www.error != null)
+        {
+            Debug.LogError("Cannot Connect to Account Creation");
+            Debug.Log(www.error);
+        }
+        else
+        {
+          string textReturn = www.text;
+            if (url == CheckDupUrl)
+            {
+                if (textReturn == "0")
+                {
+                    ActiveNum = 1;  // 중복이 아닐때 (계정 생성 가능할때)
+                }
+                else if (textReturn == "1")
+                {
+                    ActiveNum = 0;  // 중복일때
+                }
+            }
+            else if (url == CheckLoginUrl)
+            {
+                AccessNumber = textReturn;
+                AccessNumber = AccessNumber.Insert(AccessNumber.Length, "L");
+                Debug.Log(AccessNumber);
+            }
+            else if (url == CreateAccountUrl)
+            {
+                if (textReturn == "make account OK")
+                    ActiveNum = 5;  // 계정 생성 완료
+            }
+            else if (url == TakeDataUrl)
+            {
+                Debug.Log("TakeData");
+                Debug.Log(textReturn);
+                ActiveNum = 10;
+            }
+        }
+    }
     #endregion
 }
---
+++ Assets/2 Script/Multi/WebServ.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebServ : MonoBehaviour {
+
+    public string UserData;
+    public Login login;
+	// Use this for initialization
+	void Start () {
+       login= GameObject.FindObjectOfType<Login>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	if(Application.loadedLevelName == "login")
+        {
+
+            UserData = login.AccessNumber;
+            print(UserData);
+
+        }
+
+
+    }
+}
