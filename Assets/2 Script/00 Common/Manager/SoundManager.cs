08195b1139bc95bfe68260bb768169784bb8f4f7
myocon <myo_con@naver.com>
2016-08-31 01:36:09 +0900

    SoundManager추가, DepthofField는 왜 난리지

---
+++ Assets/2 Script/00 Common/Manager/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    public List<AudioSource> backgroundAudioList = new List<AudioSource>();
+    public List<AudioSource> effectAudioList     = new List<AudioSource>();
+    private int MAXSOUND = 5;
+    private int backgroundSound = 5;
+    private int effectSound = 5;
+
+    public void AddBackgroundSound(AudioSource _source)
+    {
+        backgroundAudioList.Add(_source);
+    }
+    public void AddEffectSound(AudioSource _source)
+    {
+        effectAudioList.Add(_source);
+    }
+
+    public void SetBackgroundSound(int _sound)
+    {
+        float sound = _sound / MAXSOUND;
+        if (sound > 1f)
+            sound = 1f;
+
+        for(int i = 0; i <backgroundAudioList.Count; ++i)
+        {
+            backgroundAudioList[i].volume = sound;
+        }
+    }
+
+    public void SetEffectSound(int _sound)
+    {
+        float sound = _sound / MAXSOUND;
+        if (sound > 1f)
+            sound = 1f;
+
+        for (int i = 0; i < effectAudioList.Count; ++i)
+        {
+            effectAudioList[i].volume = sound;
+        }
+    }
+}
