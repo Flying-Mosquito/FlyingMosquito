f55dbec33fc015c126ad0fe621182a2c5e36ebd5
myocon <myo_con@naver.com>
2016-10-02 01:47:04 +0900

    UI - Timer일부수정

--- Assets/2 Script/01 UI/Status/StatusUICtrl.cs
+++ Assets/2 Script/01 UI/Status/StatusUICtrl.cs
@@ -11,17 +11,23 @@ public class StatusUICtrl : MonoBehaviour {
     public Image hpImage;
     public Image staminaImage;
     public Image bloodImage;
+    public Text LimitTimeText;
+    public float fLimitTime;
+
 
     public PlayerCtrl player;
 
     void Start()
     {
+        fLimitTime = 100f;
         MAXHP = 100f;
         MAXSTAMINA = 100f;
         MAXBLOOD = 100f;
         hpImage = GameObject.Find("Heart").GetComponent<Image>();
         staminaImage = GameObject.Find("StaminaBar").GetComponent<Image>();
         bloodImage = GameObject.Find("BloodBar").GetComponent<Image>();
+        LimitTimeText = GameObject.Find("LimitTime").GetComponent<Text>();
+        LimitTimeText.text = fLimitTime.ToString();
 
         player = GameObject.FindObjectOfType<PlayerCtrl>();
 
@@ -32,7 +38,12 @@ public class StatusUICtrl : MonoBehaviour {
         SetPlayerHPBar();
         SetPlayerStaminaBar();
         SetPlayerBloodBar();
+        //System.Text.StringBuilder
+        
+     
+            SetLimitTimer();
     }
+
     public void SetPlayerHPBar()
     {
         hpImage.fillAmount = player.fHP / MAXHP;
@@ -45,6 +56,25 @@ public class StatusUICtrl : MonoBehaviour {
     public void SetPlayerBloodBar()
     {
         bloodImage.fillAmount = player.iBlood / MAXBLOOD;
+    }
+    public void SetLimitTimer()
+    {
+        if (fLimitTime < 0f)
+            return;
+
+            fLimitTime -= Time.deltaTime;
+
+        // 0~1사이는 따로 처리해줘야 할듯
+        if (fLimitTime < 0f)   // 게임오버라던가 그런거 띄우는처리 해줘야함 
+            return;
+
+        int toIntTime = (int)fLimitTime;
+
+
+
+        LimitTimeText.text = string.Format("{0:D3}", toIntTime);
+     
+
     }
 
 }
